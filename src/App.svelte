bef499f25bdafae656fb053db131b3391121f9d5
vorsakha <andreitf.dev@gmail.com>
2022-01-23 17:04:00 -0300

    starter

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<div class="container flex flex-col items-center justify-center h-screen">
+		<h1 class="text-center text-red-400 font-bold text-5xl mb-10">Hello {name}!</h1>
+		<p>This Svelte starter has Tailwindcss pre installed :)</p>
+	</div>
+</main>
